55fa4ed607f528b481e50d6a6c54783464001659
srgsuh <srg.suhoverhov@gmail.com>
2025-08-04 17:30:45 +0300

    Update morgan logging format based on `NODE_ENV` and enhance debug output

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,9 @@ import {EmployeeController} from "./controller/EmployeeController.ts";
 
 const DEFAULT_PORT = 3000;
 const port = process.env.PORT || DEFAULT_PORT;
+const morganFormat = process.env.NODE_ENV === "production"? 'tiny': 'dev';
+
+console.log("NODE_ENV=", process.env.NODE_ENV);
 
 const employeesService: EmployeesService = new EmployeesServiceMap();
 const employeeController = new EmployeeController(employeesService);
@@ -16,7 +19,7 @@ const employeeController = new EmployeeController(employeesService);
 const app = express();
 
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan(morganFormat));
 
 app.get("/employees", employeeController.getAll);
 app.get("/employees/:id", employeeController.getEmployee);
