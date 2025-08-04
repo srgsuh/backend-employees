d304c6397dfa418411e59dd9a327ef1b456030db
srgsuh <srg.suhoverhov@gmail.com>
2025-08-04 18:04:47 +0300

    Remove unused `NextFunction` parameter from `EmployeeController` methods

--- src/controller/EmployeeController.ts
+++ src/controller/EmployeeController.ts
@@ -1,29 +1,29 @@
 import EmployeesService from "../service/EmployeeService.ts";
-import {NextFunction, Request, Response} from "express";
+import {Request, Response} from "express";
 
 export class EmployeeController {
     constructor(private service: EmployeesService) {}
 
-    getAll = (req: Request, res: Response, next: NextFunction) => {
+    getAll = (_: Request, res: Response) => {
         res.json(this.service.getAll());
     }
 
-    getEmployee = (req: Request, res: Response, next: NextFunction) => {
+    getEmployee = (req: Request, res: Response) => {
         const id = req.params.id;
         res.json(this.service.getEmployee(id));
     }
 
-    addEmployee = (req: Request, res: Response, next: NextFunction) => {
+    addEmployee = (req: Request, res: Response) => {
         const employee = this.service.addEmployee(req.body);
         res.json(employee);
     }
 
-    deleteEmployee = (req: Request, res: Response, next: NextFunction) => {
+    deleteEmployee = (req: Request, res: Response) => {
         const id = req.params.id;
         res.json(this.service.deleteEmployee(id));
     }
 
-    updateEmployee = (req: Request, res: Response, next: NextFunction) => {
+    updateEmployee = (req: Request, res: Response) => {
         const id = req.params.id;
         const fields = req.body;
         res.json(this.service.updateEmployee({id, fields}));
