3737a341eca9fb1e7f259714e6ae0b4dea8d6d31
srgsuh <srg.suhoverhov@gmail.com>
2025-08-07 13:12:05 +0300

    Introduce employee validation schemas

---
+++ src/schemas/employees.schema.ts
@@ -0,0 +1,18 @@
+import z from 'zod';
+
+export const employeesFullSchema = z.object({
+    id: z.guid(),
+    salary: z.coerce.number().int().positive(),
+    fullName: z.string().max(255),
+    department: z.string().max(255),
+    birthDate: z.iso.date(),
+    avatar: z.union([
+        z.url({protocol: /^https?$/,hostname: z.regexes.domain}),
+        z.literal(""),
+    ]),
+});
+
+export const employeeCreateSchema = employeesFullSchema.partial({id: true});
+
+export type EmployeeFull = z.infer<typeof employeesFullSchema>;
+export type EmployeeCreate = z.infer<typeof employeeCreateSchema>;
