5f2c7705464a264dbf6e1b57b91760deb6098ad5
srgsuh <srg.suhoverhov@gmail.com>
2025-08-10 11:30:47 +0300

    Simplify query parsing and error handling in parseGetQuery middleware

--- src/middleware/parseGetQuery.ts
+++ src/middleware/parseGetQuery.ts
@@ -1,21 +1,17 @@
 import z from "zod";
 import {NextFunction, Request, Response} from "express";
 import _ from "lodash";
-import {parseZodError} from "../utils/zod-utils.ts";
+import {validationError} from "../utils/zod-utils.ts";
 import {searchRequestSchema} from "../schemas/get-query-parameters.schema.ts";
 
-
 export function parseGetQuery(req: Request, __: Response, next: NextFunction) {
-    console.log("PARSE GET QUERY");
     try {
         const query = req.query;
         if (query && !_.isEmpty(query)) {
             req.searchObject = searchRequestSchema.parse(query);
-            console.log("PARSE GET QUERY", JSON.stringify(req.searchObject));
         }
         next();
     } catch (e) {
-        const error = (e instanceof z.ZodError)? parseZodError(e): e;
-        next(error);
+        next(e instanceof z.ZodError? validationError(e): e);
     }
 }
