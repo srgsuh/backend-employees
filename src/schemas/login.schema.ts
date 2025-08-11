5d409b6fca78a768373dae5f7369f9073556d9a0
srgsuh <srg.suhoverhov@gmail.com>
2025-08-11 16:17:20 +0300

    Fix login schema

--- src/schemas/login.schema.ts
+++ src/schemas/login.schema.ts
@@ -2,5 +2,5 @@ import z from "zod";
 
 export const loginSchema = z.strictObject({
     email: z.email({pattern: z.regexes.html5Email}),
-    password: z.string().min(4)
+    password: z.string()
 });
