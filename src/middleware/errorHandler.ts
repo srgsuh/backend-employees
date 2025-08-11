880a4ded9727301f3817f3e1fde12a04c409f27e
srgsuh <srg.suhoverhov@gmail.com>
2025-08-11 09:26:09 +0300

    Enhance error handling with Zod error support and simplify parseGetQuery middleware

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,14 +1,19 @@
 import {Request, Response, NextFunction} from "express";
 import {EmployeeAlreadyExistsError, EmployeeNotFoundError} from "../service/EmployeeServiceErrors.ts";
+import {parseZodError} from "../utils/zod-utils.ts";
+import {ZodError, prettifyError} from "zod";
 
 export function errorHandler(error: Error, _req: Request, res: Response, _next: NextFunction) {
     let status = 400;
+
     if (error instanceof EmployeeAlreadyExistsError) {
         status = 409;
     }
     else if (error instanceof EmployeeNotFoundError) {
         status = 404;
     }
-    const message = error.message;
+
+    const message = error instanceof ZodError? prettifyError(error) :error.message;
+
     res.status(status).json({error: message});
 }
--- src/middleware/parseGetQuery.ts
+++ src/middleware/parseGetQuery.ts
@@ -1,17 +1,11 @@
-import z from "zod";
 import {NextFunction, Request, Response} from "express";
 import _ from "lodash";
-import {validationError} from "../utils/zod-utils.ts";
 import {searchRequestSchema} from "../schemas/get-query-parameters.schema.ts";
 
 export function parseGetQuery(req: Request, __: Response, next: NextFunction) {
-    try {
-        const query = req.query;
-        if (query && !_.isEmpty(query)) {
-            req.searchObject = searchRequestSchema.parse(query);
-        }
-        next();
-    } catch (e) {
-        next(e instanceof z.ZodError? validationError(e): e);
+    const query = req.query;
+    if (query && !_.isEmpty(query)) {
+        req.searchObject = searchRequestSchema.parse(query);
     }
+    next();
 }
