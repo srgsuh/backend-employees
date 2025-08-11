064074d584ec31ff222b6770cf8a07f93a1fe53a
srgsuh <srg.suhoverhov@gmail.com>
2025-08-11 09:28:47 +0300

    Remove unused ` import in validateBody middleware

--- src/middleware/validateBody.ts
+++ src/middleware/validateBody.ts
@@ -1,6 +1,5 @@
 import z from "zod";
 import {Request, Response, NextFunction} from "express";
-import {validationError} from "../utils/zod-utils.js";
 
 export default function<T extends z.ZodTypeAny>(schema: T) {
     return function (req: Request, _: Response, next: NextFunction) {
