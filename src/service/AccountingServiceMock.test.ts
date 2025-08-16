5e3d72ba1f7b5a426d6676fb81dc0056d36f43bf
srgsuh <srg.suhoverhov@gmail.com>
2025-08-16 14:37:56 +0300

    And mock accounting service

---
+++ src/service/AccountingServiceMock.test.ts
@@ -0,0 +1,9 @@
+import LoginData from '../model/LoginData.ts';
+import LoginResponse from '../model/LoginResponse.ts';
+import type AccountingService from './AccountingService.ts';
+
+export class AccountingServiceMock implements AccountingService {
+    login(_: LoginData): LoginResponse {
+        return {} as LoginResponse;
+    }
+}
