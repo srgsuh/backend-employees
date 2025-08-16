9328eeccf72285e51fc22133087a4239749dabcb
srgsuh <srg.suhoverhov@gmail.com>
2025-08-16 14:36:56 +0300

    Add support to disable morgan logging through environment variable

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import {errorHandler} from "./middleware/errorHandler.ts";
 import {defaultHandler} from "./middleware/defaultHandler.ts";
 import {createWriteStream} from "node:fs";
 import path from "node:path";
-import {getEnvIntVariable, getEnvVariable} from "./utils/env-utils.ts";
+import {getEnvBoolVariable, getEnvIntVariable, getEnvVariable} from "./utils/env-utils.ts";
 import employeeRouter from "./routes/employees-router.ts";
 import loginRouter from "./routes/login-router.ts";
 import {authenticate} from "./middleware/auth/authenticate.ts";
@@ -18,13 +18,15 @@ const morganFormat = getEnvVariable("MORGAN_FORMAT", DEFAULT_MORGAN_FORMAT);
 const morganSkip = getEnvIntVariable("MORGAN_SKIP", DEFAULT_MORGAN_SKIP_THRESHOLD);
 const morganFile = process.env.MORGAN_FILE;
 const logDir = getEnvVariable("LOG_DIR", DEFAULT_LOG_DIR);
+const morganOff= getEnvBoolVariable("MORGAN_OFF", false);
 
 const app = express();
 
 app.use(cors());
-app.use(morgan(morganFormat, {skip: (__, res) => res.statusCode < morganSkip}));
-morganFile && app.use(morgan('combined', {stream: createWriteStream(path.join(logDir, morganFile), {flags: 'a'})}));
-
+if (!morganOff) {
+    app.use(morgan(morganFormat, {skip: (__, res) => res.statusCode < morganSkip}));
+    morganFile && app.use(morgan('combined', {stream: createWriteStream(path.join(logDir, morganFile), {flags: 'a'})}));
+}
 app.use("/employees", authenticate);
 app.use(express.json());
 
