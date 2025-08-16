fe8de0db4ec517299413886866dac3fa0e922ea1
srgsuh <srg.suhoverhov@gmail.com>
2025-08-16 14:36:36 +0300

    Refactor service factory methods to support mock implementations based on environment variable

--- src/service/services.ts
+++ src/service/services.ts
@@ -7,18 +7,25 @@ import {FileStorage} from "./FileStorage.ts";
 import type AccountingService from "./AccountingService.ts";
 import { AccountingServiceMap } from "./AccountingServiceMap.ts";
 import Persistable, { isPersistable } from "./Persistable.ts";
+import { AccountingServiceMock } from "./AccountingServiceMock.test.ts";
 
 export function getEmployeeService(): EmployeeService {
+    console.log("process.env.NODE_TEST_CONTEXT: ", process.env.NODE_TEST_CONTEXT);
+    console.log("process.env.NODE_ENV: ", process.env.NODE_ENV);
     if (process.env.NODE_TEST_CONTEXT) {
+        console.log("Using mock EmployeeService");
         return new EmployeeServiceMock();
-    } else {
-        return new EmployeesServiceMap(
-            new FileStorage<Employee>(employeeSchemaLoad, process.env.DB_FILE_PATH)
-        );
     }
+    console.log("Using EmployeesServiceMap EmployeeService");
+    return new EmployeesServiceMap(
+        new FileStorage<Employee>(employeeSchemaLoad, process.env.DB_FILE_PATH)
+    );
 }
 
 export function getAccountingService(): AccountingService {
+    if (process.env.NODE_TEST_CONTEXT) {
+        return new AccountingServiceMock();
+    }
     return new AccountingServiceMap();
 }
 
