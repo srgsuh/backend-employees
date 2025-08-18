cfc395d76b603c4a4e82ea0693fd4727b881a733
srgsuh <srg.suhoverhov@gmail.com>
2025-08-18 10:35:50 +0300

    Minor import fixes

--- src/service/AccountingServiceMock.test.ts
+++ src/service/AccountingServiceMock.test.ts
@@ -2,7 +2,6 @@ import LoginData from '../model/LoginData.ts';
 import LoginResponse from '../model/LoginResponse.ts';
 import type AccountingService from './AccountingService.ts';
 import {accountingServiceRegistry} from "./registry.ts";
-import {AccountingServiceMap} from "./AccountingServiceMap.js";
 
 export class AccountingServiceMock implements AccountingService {
     login(_: LoginData): LoginResponse {
